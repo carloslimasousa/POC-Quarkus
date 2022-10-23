936b49f6a99d0dfc5da4c704107412a7ccd7d20f
carloslimasousa <79268209+carloslimasousa@users.noreply.github.com>
2022-10-23 19:25:17 -0300

    init project

---
+++ first-app-quarkus/src/native-test/java/poc/c6bak/quarkus/carlos/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package poc.c6bak.quarkus.carlos;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
